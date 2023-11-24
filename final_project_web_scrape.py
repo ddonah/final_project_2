d3579ea9e6b58b7926de57bfa1f624a1552076da
ddonah <129772074+ddonah@users.noreply.github.com>
2023-11-24 11:51:02 -0700

    Update final_project_web_scrape.py

--- final_project_web_scrape.py
+++ final_project_web_scrape.py
@@ -1,8 +1,23 @@
 
 import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
-def get_name_data(state, year):
+def get_name_data(stte, yr):
+    r1 = requests.get()
+    selection = {
+        'state': stte,
+        'year': yr
+    }
+    r = requests.post('https://www.ssa.gov/cgi-bin/namesbystate.cgi', data=selection)
+    df = pd.read_html(r.text)[1].iloc[4:,:].reset_index()
+    f_name_df = df.iloc[:, 4:6]
+    m_name_df = df.iloc[:, 2:4]
+    return f_name_df, m_name_df
+
     
+    
+
 
 
 
