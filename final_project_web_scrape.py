76b48d18f6bbf27b96bf54e666cc505c41f57d59
ddonah <129772074+ddonah@users.noreply.github.com>
2023-11-24 14:31:11 -0700

    Update final_project_web_scrape.py

--- final_project_web_scrape.py
+++ final_project_web_scrape.py
@@ -1,19 +1,32 @@
-
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-def get_name_data(stte, yr):
-    r1 = requests.get()
-    selection = {
-        'state': stte,
-        'year': yr
-    }
-    r = requests.post('https://www.ssa.gov/cgi-bin/namesbystate.cgi', data=selection)
-    df = pd.read_html(r.text)[1].iloc[4:,:].reset_index()
-    f_name_df = df.iloc[:, 4:6]
-    m_name_df = df.iloc[:, 2:4]
-    return f_name_df, m_name_df
+def get_name_data(stte):
+    r1 = requests.get('https://www.ssa.gov/OACT/babynames/state/index.html')
+    soup = BeautifulSoup(r1.content)
+    yrs = [x['value'] for x in soup.find('select', {'name': 'year'}).find_all('option')]
+    name_list = []
+    for yr in yrs:
+        selection = {
+            'state': stte,
+            'year': yr
+        }
+        r2 = requests.post('https://www.ssa.gov/cgi-bin/namesbystate.cgi', data=selection)
+        df = pd.read_html(r2.text)[1].iloc[4:,:].reset_index()
+        f_name_df = df.iloc[:, 4:6]
+        f_name_df.columns = ['name', yr]
+        f_name_df['gender'] = 'female'
+        m_name_df = df.iloc[:, 2:4]
+        m_name_df.columns = ['name', yr]
+        m_name_df['gender'] = 'male'
+        name_df = pd.concat([f_name_df, m_name_df], ignore_index=True)
+        name_list.append(name_df)
+    final_name_df = name_list[0][['name', 'gender', '2022']]
+    for el in name_list[1:]:
+        final_name_df = final_name_df.merge(el, on=['name', 'gender'], how='outer')
+    df['total'] = df.loc[:, '2022':'1960'].apply(pd.to_numeric).sum(axis=1, skipna=True)
+    return final_name_df
 
     
     
