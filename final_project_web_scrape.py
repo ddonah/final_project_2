914309b167f001334e311fdd14750b782b9eefc0
ddonah <129772074+ddonah@users.noreply.github.com>
2023-11-25 18:41:08 -0700

    Update final_project_web_scrape.py

--- final_project_web_scrape.py
+++ final_project_web_scrape.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.patches import Patch  
 
 def get_name_data(stte):
     r1 = requests.get('https://www.ssa.gov/OACT/babynames/state/index.html')
@@ -27,6 +29,31 @@ def get_name_data(stte):
         final_name_df = final_name_df.merge(el, on=['name', 'gender'], how='outer')
     df['total'] = df.loc[:, '2022':'1960'].apply(pd.to_numeric).sum(axis=1, skipna=True)
     return final_name_df
+
+def figure_1(df):
+    names_2022 = list(df.sort_values('2022', ascending = False).iloc[0:5, :].name.values)
+    values_2022 = list(df.sort_values('2022', ascending = False).iloc[0:5, :]['2022'].values)
+    year_2022 = [2022]*5
+    names_1960 = list(df.sort_values('1960', ascending = False).iloc[0:5, :].name.values)
+    values_1960 = list(df.sort_values('1960', ascending = False).iloc[0:5, :]['1960'].values)
+    year_1960 = [1960]*5
+    all_names = names_2022 + names_1960
+    all_values = values_2022 + values_1960
+    all_years = year_2022 + year_1960
+    n_df = pd.DataFrame(data={
+        'name': all_names,
+        'count' : all_values,
+        'year' : all_years
+    })
+    plt.bar(x = n_df[n_df.year == 2022].name.values, height = n_df[n_df.year == 2022].population.values, color = 'darkblue')
+    plt.bar(x = n_df[n_df.year == 1960].name.values, height = n_df[n_df.year == 1960].population.values, color = 'orange')
+    plt.xticks(rotation=45, ha='right')
+    legend_info = [Patch(color='orange', label='1960'), 
+                Patch(color='darkblue', label='2022')]
+    plt.legend(handles=legend_info)
+    plt.ylabel('Number of babies', fontsize = 16.0)
+    plt.title('Number of children named the top 5 most \npopular names in 1960 & 2022', fontsize = 18.0)
+    return None
 
     
     
